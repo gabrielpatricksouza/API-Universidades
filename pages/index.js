83a329976bdeb230d69b59a33c3bd8c2ba87a1c6
gabrielpatricksouza <gabrielpatricksouza777@gmail.com>
2021-10-18 16:53:45 -0300

    Adicionado Home(index), Adicionado o projeto para documentação web(flutter)

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,32 @@
-function Home(){
-    return <div>Em breve a documentação</div>
+function Home() {
+    return (
+        <div>
+            <head>
+                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
+            </head>
+            <body>
+                <div>
+                    <div class="card">
+                        <img src="https://raw.githubusercontent.com/gabrielpatricksouza/gabrielpatricksouza/master/foto_perfil.jpg" />
+                        <h1 class="title">Gabriel Patrick</h1>
+                        <p>Sofware Developer</p>
+
+                        <form action="#">
+                            <button class="btn" type="submit" >Documentação</button>
+                        </form>
+                    </div>
+
+                    <div class="icons">
+
+                        <a href="https://www.instagram.com/gabrielpatricksouza/"><i class="fa fa-instagram fa-2x"></i></a>
+                        <a href="https://api.whatsapp.com/send?phone=5538991656566"><i class="fa fa-whatsapp fa-2x"></i></a>
+                        <a href="https://www.linkedin.com/in/gabriel-patrick-8132b7153/"><i class="fa fa-linkedin fa-2x"></i></a>
+                        <a href="https://github.com/gabrielpatricksouza"><i class="fa fa-github fa-2x"></i></a>
+                    </div>
+                </div>
+            </body>
+        </div>
+    );
 }
 
 export default Home
